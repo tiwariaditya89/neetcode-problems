05a6c483a2652fd028b5cfbc5ff7ef1ee1b6b382
tiwariaditya89 <adityakumartiwari564@gmail.com>
2025-05-03 11:23:56 +0530

    15. 3Sum

---
+++ ThreeSum.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        int target = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1, right = nums.length - 1;
+            int x = nums[i];
+
+            while (left < right) {
+                int sum = x + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(x, nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+}

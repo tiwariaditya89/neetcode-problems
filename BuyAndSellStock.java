1aa9a2fbce8f69421be36bfb1bd67d9c3db66c32
tiwariaditya89 <adityakumartiwari564@gmail.com>
2025-05-06 22:36:00 +0530

    121. Best Time to Buy and Sell Stock

---
+++ BuyAndSellStock.java
@@ -0,0 +1,11 @@
+public class BuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+        return maxProfit;
+    }
+}
